c3e5f898b53a8bb94aabf0157506fac5b8c1c4ff
Yotman Reyes <yotman.reyes@gmail.com>
2025-10-08 13:59:51 -0400

    allow Restful methods and special headers

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ const corsOptions = {
     'http://localhost:5173' 
   ],
   credentials: true,
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 // Middleware
